8efd0da3719768ec8fe931d961ec28e822183c68
AlbertoVf <avf.git-repos@protonmail.com>
2023-07-13 17:59:10 +0200

    ✨ feat: update desktop

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,15 @@
+tint2 &
+variety & # run
+plank &
+xfce4-power-manager &
+xfce4-clipman &
+numlockx on &
+volumeicon &
+nm-applet &
+blueberry-tray &
+pamac-tray &
+sxhkd -c $HOME/.config/sxhkd/sxhkdrc &
+picom --config $HOME/.config/picom/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+(conky -c $HOME/.conkyrc) &
