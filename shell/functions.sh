d017d30effe878f8cb68d7e8b26f968d11fe0621
AlbertoVf <avf.git-repos@protonmail.com>
2024-06-14 16:49:59 +0200

    feat: new functions

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 python_environment() {
+	# build python environment and activate.
+	# Install linter, formatter and test
+	# Install required packages
+
 	echo '🏗️ Build environment'
 	python -m venv .venv
 	echo '🏃 run environment'
@@ -10,15 +14,30 @@ python_environment() {
 }
 
 add_submodule() {
+	# add submodule with github url.
+	# Submodule dest folder is the submodule name
+
 	module=$1
 	name=$(echo $module | rev | cut -d'/' -f1 | rev)
-
 	name=$(echo $name | sed 's/\.git//g')
 
 	git submodule add $module $name
 }
 
-export_packages(){
-	echo "# Packages installed on $(uname -n) on $(date +'%Y-%m-%d %H.%M.%S')"> ~/packages-installes.txt
-	pacman -Qq >> ~/packages-installes.txt
+export_packages() {
+	# export archlinux packages to file
+	echo "# Packages installed on $(uname -n) on $(date +'%Y-%m-%d %H.%M.%S')" >~/packages-installes.txt
+	pacman -Qq >>~/packages-installes.txt
+}
+
+window_title() {
+	# get the window title and save on clipboard
+	xdotool getwindowname $(xdotool selectwindow) | xsel -bi
+	notify-send "Titulo de ventana copiado a portapapeles: $(xclip -o -selection clipboard)"
+}
+
+window_class() {
+	# get the window class and save on clipboard
+	xprop -id $(xdotool selectwindow) WM_CLASS | xsel -bi
+	notify-send "Clase de ventana copiado a portapapeles: $(xclip -o -selection clipboard)"
 }
