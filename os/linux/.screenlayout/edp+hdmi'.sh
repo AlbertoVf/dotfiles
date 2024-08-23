e4134007fc4a8fa6ea7abbabdc82f352b15e0505
AlbertoVf <avf.git-repos@protonmail.com>
2024-08-23 16:08:53 +0200

    refactor: rename files

--- os/linux/.screenlayout/edp+hdmi'.sh
+++ os/linux/.screenlayout/edp+hdmi'.sh
@@ -3,4 +3,4 @@ xrandr \
 --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal \
 --output DP-1 --off \
 --output HDMI-1 --off \
---output HDMI-2 --off
+--output HDMI-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
